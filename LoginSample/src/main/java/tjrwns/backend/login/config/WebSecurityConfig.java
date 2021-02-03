5be1403cb9af77a0d13152b7531707ce8816de73
윤석준 <tjrwns8024@naver.com>
2021-02-03 19:30:11 +0900

    [ADD] <config/ WebSecurityConfig.java> add annotation

--- LoginSample/src/main/java/tjrwns/backend/login/config/WebSecurityConfig.java
+++ LoginSample/src/main/java/tjrwns/backend/login/config/WebSecurityConfig.java
@@ -1,19 +1,17 @@
 package tjrwns.backend.login.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
